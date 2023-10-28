64e301aaa2f97977cbfc9407290bedb5e3d70e13
Tomoki826 <uda.tomoki@icloud.com>
2023-10-28 11:40:01 +0900

    issue完了 #6

--- src/engine/main.py
+++ src/engine/main.py
@@ -6,20 +6,22 @@ from pydantic import BaseModel
 app = FastAPI()
 
 # リクエストのBaseModelを作成
-class PageOpen(BaseModel):
+class OnPageOpen(BaseModel):
     content: str
 
-class GoogleSearch(BaseModel):
+class OnGoogleSearch(BaseModel):
     query: str
 
+# ページ内容から悩み続けているか判定
 @app.post("/on_page_open")
-async def on_page_open(item: PageOpen):
-    print(item.content)
+async def on_page_open(req: OnPageOpen):
+    print(req.content)
     response_data = {"message": "This is a successful response."}
     return JSONResponse(content=response_data, status_code=200)
 
+# 検索ワードから悩み続けているか判定
 @app.post("/on_google_search")
-async def on_google_search(item: GoogleSearch):
-    print(item.query)
+async def on_google_search(req: OnGoogleSearch):
+    print(req.query)
     response_data = {"message": "This is a successful response."}
     return JSONResponse(content=response_data, status_code=200)
