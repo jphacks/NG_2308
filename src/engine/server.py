ee5359e72bb8061f49847b9506394d0941b34ce8
Tomoki826 <uda.tomoki@icloud.com>
2023-10-28 13:13:09 +0900

    完成 #6

--- src/engine/server.py
+++ src/engine/server.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-
+# fastapiを実行
 app = FastAPI()
 
-# 適当なダミーのエンドポイント
-@app.post("/test/{id}")
-async def test(id: str,):
-    print(f"got request {id}")
+# BaseModelをインポート
+from src.engine.model import OnPageOpen, OnGoogleSearch
+
+# ページ内容から悩み続けているか判定
+@app.post("/on_page_open")
+async def on_page_open(req: OnPageOpen):
+    print(req.content)
+    response_data = {"message": "This is a successful response."}
+    return JSONResponse(content=response_data, status_code=200)
+
+# 検索ワードから悩み続けているか判定
+@app.post("/on_google_search")
+async def on_google_search(req: OnGoogleSearch):
+    print(req.query)
+    response_data = {"message": "This is a successful response."}
+    return JSONResponse(content=response_data, status_code=200)
