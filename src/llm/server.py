68b0947c71fec3e3ddc0342047a246d10659bb34
Lucas544875 <takayanagi691@gmail.com>
2023-10-29 11:53:35 +0900

    [update] server.py で仮の動作をLLMを利用する動作に置きかえ #51

--- src/llm/server.py
+++ src/llm/server.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uuid, random
 
+from LLM import Agent
+
 # fastapiを実行
 app = FastAPI()
 
@@ -24,14 +26,14 @@ from models import OnAction
 # 自然言語モデルに対する処理
 @app.post("/on_action")
 async def on_action(req: OnAction):
-    client_uuid = req.client_uuid
+    client_uuid_str = str(req.client_uuid)
     # 空文字列を受け取るとuuidを新規発行
-    if client_uuid == "":
-        client_uuid = str(uuid.uuid4())
-        agent_dict[client_uuid] = None
+    if client_uuid_str == "":
+        client_uuid_str = str(uuid.uuid4())
+        agent_dict[client_uuid_str] = Agent()
 
-    # (仮の処理)
-    random_bool = random.choice([True, False])
-    response_data = {"is_same_topic": random_bool, "client_uuid": client_uuid}
+
+    is_same_topic = agent_dict[client_uuid_str].on_user_action(req)
+    response_data = {"is_same_topic": is_same_topic, "client_uuid": client_uuid_str}
 
     return JSONResponse(content=response_data, status_code=200)
